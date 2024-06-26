4b346a67a92461795766aac51c804122eb2bdd57
johndbs <83272366+johndbs@users.noreply.github.com>
2024-06-26 16:00:34 +0200

    feat: Refactor App.vue to use computed property for answers

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
-  <div>
-    <h1>jhfsdkjsndfsnflksflksf ksnfksfdlkjfsjlfslk</h1>
+  <div v-if="this.question">
+    <h1 v-html="this.question"></h1>
 
-    <input type="radio" name="options" value="True">
-    <label>True</label><br>
-
-    <input type="radio" name="options" value="False">
-    <label>False</label><br>
+    <template v-for="(answer, index) in answers" :key="index">
+      <input type="radio" name="options" value="answer">
+      <label v-html="answer"></label><br>
+    </template>
 
     <button class="send" type="button"> Send</button>
 
@@ -16,6 +15,7 @@
 </template>
 
 <script lang="ts">
+import { computed } from 'vue';
 import { Options, Vue } from 'vue-class-component';
 
 @Options({
@@ -24,11 +24,31 @@ import { Options, Vue } from 'vue-class-component';
   },
 })
 export default class App extends Vue {
+  question = ''; 
+  incorrectAnswers: string[] = [];
+  correctAnswer = '';
+
+  answers = computed(()=>{
+      let answers: string[] = JSON.parse(JSON.stringify(this.incorrectAnswers)); 
+      answers.splice(Math.round(Math.random() * this.correctAnswer.length), 
+        0, 
+        this.correctAnswer);
+      return answers;
+  });
+
+
   created(): void {
       this.axios.get("https://opentdb.com/api.php?amount=1&difficulty=easy").then((response) => {
-        console.log(response.data.results[0]);
+        console.log(response.data);
+        this.question = response.data.results[0].question;
+        this.incorrectAnswers = response.data.results[0].incorrect_answers;
+        this.correctAnswer = response.data.results[0].correct_answer;
+      }).catch((error)=>{
+        console.error(error);
       });
   }
+
+
 
 }
 </script>
